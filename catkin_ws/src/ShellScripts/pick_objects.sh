873017732d6aee38e960234f74c3d8fd9fc06915
alice <bow1226@gmail.com>
2019-09-03 14:48:54 +0800

    feat: Add wall_follower_room.yaml

--- catkin_ws/src/ShellScripts/pick_objects.sh
+++ catkin_ws/src/ShellScripts/pick_objects.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/room.world" &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/World/wall_room_follower.yaml" & 
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/World/wall_follower_room.yaml" & 
 sleep 5
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
