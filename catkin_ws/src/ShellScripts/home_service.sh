18471ef566771347a44e7e150bf602f8a981b269
alice <bow1226@gmail.com>
2019-09-03 10:56:27 +0800

    test: Add test shell scripts

---
+++ catkin_ws/src/ShellScripts/home_service.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/gan/catkin_ws/src/World/room.world" &
+sleep 5
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/gan/catkin_ws/src/World/wall_room_follower.yaml" & 
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+xterm  -e  " rosrun pick_objects pick_objects " &
+sleep 5
+xterm  -e  " rosrun add_markers add_markers "
+
+
---
+++ catkin_ws/src/ShellScripts/navigation_test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/room.world" &
+sleep 5
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/World/wall_room_follower.yaml" & 
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " 
