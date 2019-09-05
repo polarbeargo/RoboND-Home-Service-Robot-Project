b489134a50042144153b37f8e7ca5cfab5d3fb28
alice <bow1226@gmail.com>
2019-09-05 15:36:56 +0800

    test: Format

--- catkin_ws/src/ShellScripts/home_service.sh
+++ catkin_ws/src/ShellScripts/home_service.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/room.world" &
-sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/World/room.yaml" & 
-sleep 5
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
-sleep 5
-xterm  -e  " rosrun pick_objects pick_objects " &
-sleep 5
-xterm  -e  " rosrun add_markers add_markers_node "
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/room.world" & sleep 5
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/World/room.yaml" & sleep 5
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " & sleep 5
+xterm -e " rosrun pick_objects pick_objects " & sleep 5
+xterm -e " rosrun add_markers add_markers_node"
 
 
