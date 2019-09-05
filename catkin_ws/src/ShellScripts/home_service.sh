84d0779752bcad58efb9dace8de845f49d21e819
Hsin-Wen Chang <bow1226@gmail.com>
2019-09-05 08:30:35 +0800

    test: Fix home_service.sh

--- catkin_ws/src/ShellScripts/home_service.sh
+++ catkin_ws/src/ShellScripts/home_service.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/gan/catkin_ws/src/World/room.world" &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/gan/catkin_ws/src/World/wall_follower_room.yaml" & 
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/gan/catkin_ws/src/World/room.yaml" & 
 sleep 5
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
