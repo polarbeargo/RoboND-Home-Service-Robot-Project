c55b6444ade2537ae461089d08739280792f1574
Hsin-Wen Chang <bow1226@gmail.com>
2019-09-05 09:37:39 +0800

    test: Use rospack find to find world file

--- catkin_ws/src/ShellScripts/test_slam.sh
+++ catkin_ws/src/ShellScripts/test_slam.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/room.world" &
+export TURTLEBOT_GAZEBO_WORLD_FILE=$(rospack find world)/room.world
+
+terminator -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " & 
+terminator -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
+sleep 2
+terminator -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
-sleep 5
-xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch"
+terminator -e "roslaunch turtlebot_teleop keyboard_teleop.launch"
