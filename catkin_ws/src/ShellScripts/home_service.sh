1de2c8c4e0a747f3a31ce56f620443d80c66205b
Hsin-Wen Chang <bow1226@gmail.com>
2019-09-15 15:54:03 +0800

    test: Change node name in home_service.sh

--- catkin_ws/src/ShellScripts/home_service.sh
+++ catkin_ws/src/ShellScripts/home_service.sh
@@ -3,6 +3,6 @@ xterm -e " source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.
 xterm -e " source devel/setup.bash ; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/World/room.yaml" & sleep 5
 xterm -e " source devel/setup.bash ; roslaunch turtlebot_rviz_launchers view_navigation.launch " & sleep 5
 xterm -e " source devel/setup.bash ; rosrun pick_objects pick_objects " & sleep 5
-xterm -e " source devel/setup.bash ; rosrun add_markers add_markers_node"
+xterm -e " source devel/setup.bash ; rosrun add_markers add_markers"
 
 
