3efe7cfc43d45e4e830ecf223c2fddb0610129c2
Hsin-Wen Chang <bow1226@gmail.com>
2019-09-21 21:21:49 -0700

    chore: Change execute order

--- catkin_ws/src/ShellScripts/home_service.sh
+++ catkin_ws/src/ShellScripts/home_service.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-xterm -e " source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/room.world" & sleep 5
-xterm -e " source devel/setup.bash ; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/World/room.yaml" & sleep 5
+xterm -e " source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/robond/RoboND-Home-Service-Robot-Project/catkin_ws/src/World/room.world" & sleep 5
+xterm -e " source devel/setup.bash ; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/robond/RoboND-Home-Service-Robot-Project/catkin_ws/src/World/room.yaml" & sleep 5
 xterm -e " source devel/setup.bash ; roslaunch turtlebot_rviz_launchers view_navigation.launch " & sleep 5
-xterm -e " source devel/setup.bash ; rosrun pick_objects pick_objects " & sleep 5
-xterm -e " source devel/setup.bash ; rosrun add_markers add_markers"
-
-
+xterm -e " source devel/setup.bash ; rosrun add_markers add_markers" & sleep 5
+xterm -e " source devel/setup.bash ; rosrun pick_objects pick_objects "
